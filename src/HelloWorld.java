683d6f4b803569ba1e36da0c2f4b2e510e1616ee
Mael Chiaverini <mael.chiaverini@epsi.fr>
2016-06-03 14:05:53 +0200

    change

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
+
+		System.out.println("Bloup");
+
         System.out.println("Hello World !");
+
+        leo est une erreur;
     }
 }
